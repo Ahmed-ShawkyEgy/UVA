ffec05e56abb6a8e84db2916f646ca32be2cd490
Ahmed-ShawkyEgy <ahmed.shawkyegy@gmail.com>
2016-09-07 15:09:33 +0200

    Create 11044 - Searching for Nessy.java

---
+++ 11044 - Searching for Nessy.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+	public static void main(String[] args) throws Throwable {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter out = new PrintWriter(System.out);
+		int t = Integer.parseInt(br.readLine());
+		while(t-->0)
+		{
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			out.append((a/3)*(b/3)+"\n");
+		}
+		out.flush();
+	}
+}
