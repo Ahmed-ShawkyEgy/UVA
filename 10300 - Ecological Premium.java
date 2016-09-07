c85e8bf1f72937827bbd92c26a4718b6de87456b
Ahmed-ShawkyEgy <ahmed.shawkyegy@gmail.com>
2016-09-07 21:19:23 +0200

    Create 10300 - Ecological Premium.java

---
+++ 10300 - Ecological Premium.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+	public static void main(String[] args) throws Throwable {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter out = new PrintWriter(System.out);
+		int t = Integer.parseInt(br.readLine());
+		while(t-->0)
+		{
+			int n = Integer.parseInt(br.readLine());
+			long ans = 0;
+			while(n-->0)
+			{
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				long cur = a*c;
+				ans+=cur;
+			}
+			out.append(ans+"\n");
+		}
+		out.flush();
+	}
+}
