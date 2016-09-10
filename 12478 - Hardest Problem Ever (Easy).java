27b83b315c6aa1a13be8d178b52cd62ba31c4039
Ahmed-ShawkyEgy <ahmed.shawkyegy@gmail.com>
2016-09-10 13:39:38 +0200

    Create 12478 - Hardest Problem Ever (Easy).java

---
+++ 12478 - Hardest Problem Ever (Easy).java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class Main{
+	
+
+	public static void main(String[] args) throws FileNotFoundException {
+		System.out.println("KABIR");
+	  }	
+}
