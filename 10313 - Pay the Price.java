e570c645c157ed8e42ccbef1ebfb8edec89f73c3
Ahmed Shawky <ahmed.shawkyegy@gmail.com>
2017-06-21 02:00:11 +0200

    Update 10313 - Pay the Price.java

--- 10313 - Pay the Price.java
+++ 10313 - Pay the Price.java
@@ -1,60 +1,62 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main{
-	public static void generate(long[][] arr)
+public class Main {
+	public static void main(String[] args) throws Throwable
 	{
-		for (int i = 1; i < arr.length; i++)
-		{
-			for (int j = 0; j < arr[i].length; j++) 
-			{
-				long dif = j-i;
-				if(dif<0)
-					dif = arr[i-1][j];
-				else
-					dif = arr[i][(int)dif]+arr[i-1][j];
-				arr[i][j] = dif;
-				
-			}
-		}
-	}
-	public static void main(String[] args) throws IOException
-    {		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter out = new PrintWriter(System.out);
-		long[][] arr = new long[301][301];
-		arr[0][0] = 1;	
-		generate(arr);
+		long[][]memo = new long[301][301];
+		memo[0][0] = 1;
+		for(int i = 1;i<301;i++)
+		{
+			for (int j = 0; j < 301; j++) 
+			{
+				int d = j-i;
+				if(d<0)
+					memo[i][j] = memo[i-1][j];
+				else
+					memo[i][j] = (long) memo[i-1][j] + (long) memo[i][d];
+			}
+		}
 		while(br.ready())
-		{	
+		{
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
-			int l1 = -1, l2 = -1;
-			if(st.hasMoreTokens()) l1 = Integer.parseInt(st.nextToken());
-			if(st.hasMoreTokens()) l2 = Integer.parseInt(st.nextToken());
-			if(l1>300)
-				l1 = 300;
-			if(l2>300)
-				l2 = 300;
-			if(l1!=-1)
+			int l1 = -1,l2 = -1;
+			if(st.hasMoreTokens())
+				l1 = Math.min(300, Integer.parseInt(st.nextToken()) );
+			if(st.hasMoreTokens())
+				l2 = Math.min(300, Integer.parseInt(st.nextToken()) );
+			if(l1 ==-1)
 			{
-				if((l2!=-1))
-				{		
-					if(l1!=0)				
-						out.append((arr[l2][n]-arr[l1-1][n])+"\n");
-					else
-						out.append((arr[l2][n])+"\n");
-				}
-				else
-				{
-					out.append(arr[l1][n]+"\n");
-				}
+				out.append(memo[n][n]+"\n");
 			}
 			else
 			{
-				out.append(arr[n][n]+"\n");
+				if(l2==-1)
+				{
+					out.append(memo[l1][n]+"\n");
+				}
+				else
+				{
+					if(l1>0)
+					{
+						out.append(memo[l2][n]-memo[l1-1][n]+"\n");
+					}
+					else
+					{
+						out.append(memo[l2][n]+"\n");
+					}
+				}
 			}
-		}		
+		}
 		out.flush();
-    }	
+	}
 }
