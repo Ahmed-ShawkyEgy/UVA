584dd180953821b4bd2899c876220dd491146707
Ahmed Shawky <ahmed.shawkyegy@gmail.com>
2018-01-26 21:33:04 +0200

    Create 10783 - Odd Sum.java

---
+++ 10783 - Odd Sum.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException 
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int cases = 1;
+		while(t-->0)
+		{
+			int a = sc.nextInt() , b = sc.nextInt();
+			int ans = 0;
+			for (int i = a; i <= b; i++) 
+				if(i%2==1)ans+=i;
+			
+			System.out.println("Case "+cases+++": "+ans);
+		}
+	}
+	
+	static class Scanner 
+	{
+		StringTokenizer st;
+		BufferedReader br;
+		public Scanner(InputStream s){	br = new BufferedReader(new InputStreamReader(s));}
+		public Scanner(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(s));
+		}
+		public String next() throws IOException 
+		{
+			while (st == null || !st.hasMoreTokens()) 
+				st = new StringTokenizer(br.readLine());
+			return st.nextToken();
+		}
+		public int nextInt() throws IOException {return Integer.parseInt(next());}
+		public String nextLine() throws IOException {return br.readLine();}		
+		public long nextLong() throws IOException {return Long.parseLong(next());}		
+		public double nextDouble() throws IOException {return Double.parseDouble(next());}	
+		public boolean ready() throws IOException {return br.ready();}
+		public int[] nextIntArr() throws IOException{
+			st = new StringTokenizer(br.readLine());
+			int[] res = new int[st.countTokens()];
+			for (int i = 0; i < res.length; i++) 
+				res[i] = Integer.parseInt(st.nextToken());
+			
+			return res;
+		}
+		public char[] nextCharArr() throws IOException{
+			st = new StringTokenizer(br.readLine());
+			char[] res = new char[st.countTokens()];
+			for (int i = 0; i < res.length; i++) 
+				res[i] = st.nextToken().charAt(0);
+			
+			return res;
+		}
+	}	
+	
+}
