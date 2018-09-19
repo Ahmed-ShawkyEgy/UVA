f788c279fb83ab52e3ce07018457e7825bac2225
Ahmed Shawky <ahmed.shawkyegy@gmail.com>
2018-09-19 23:12:55 +0200

    Create 10056 - What is the Probability ?.java

---
+++ 10056 - What is the Probability ?.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	
+	public static void main(String[] args) throws Throwable {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+			int n = sc.nextInt();
+			double p = sc.nextDouble(),q = 1-p;
+			int i = sc.nextInt();
+			double pow = Math.pow(q, n);
+			double sum = pow / (1-pow);
+			if(q==1)
+				System.out.println("0.0000");
+			else
+				System.out.printf("%.4f\n",Math.pow(q, i-1)*p*(1+sum));
+		}
+	}
+	
+
+
+	static class Scanner 
+	{
+		StringTokenizer st;
+		BufferedReader br;
+		public Scanner(InputStream s){	br = new BufferedReader(new InputStreamReader(s));}
+		public Scanner(String s) throws FileNotFoundException {br = new BufferedReader(new FileReader(s));}
+		public String next() throws IOException 
+		{
+			while (st == null || !st.hasMoreTokens()) 
+				st = new StringTokenizer(br.readLine());
+			return st.nextToken();
+		}
+		public int nextInt() throws IOException {return Integer.parseInt(next());}
+		public String nextLine() throws IOException {return br.readLine();}		
+		public long nextLong() throws IOException {return Long.parseLong(next());}		
+		public double nextDouble() throws IOException {return Double.parseDouble(next());}	
+		public char nextChar() throws IOException{return next().charAt(0);}
+		public boolean ready() throws IOException {return br.ready();}
+		public int[] nextIntArr() throws IOException{
+			st = new StringTokenizer(br.readLine());
+			int[] res = new int[st.countTokens()];
+			for (int i = 0; i < res.length; i++)
+				res[i] = nextInt();
+			return res;
+		}
+		public char[] nextCharArr() throws IOException{
+			st = new StringTokenizer(br.readLine());
+			char[] res = new char[st.countTokens()];
+			for (int i = 0; i < res.length; i++) 
+				res[i] = nextChar();
+			return res;
+		}
+	}
+
+}
